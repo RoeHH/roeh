09f92036503e03d63b4b3023494b43bb551cf91d
iccee0 <66622055+RoeHH@users.noreply.github.com>
2021-05-26 14:57:49 +0200

    Update mod.ts

--- public/mod.ts
+++ public/mod.ts
@@ -1,2 +1,1 @@
-import * as Application from "https://bitbucket.org/iccee0/calc/raw/c9045cfde50dc04b9bcbb14476a91a9defb093dd/calc.ts";
-export Application;
+import * from "https://bitbucket.org/iccee0/calc/raw/c9045cfde50dc04b9bcbb14476a91a9defb093dd/calc.ts";
